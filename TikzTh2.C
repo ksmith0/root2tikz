8213f011f8f3332992394521b945bb23e9b0e9d1
Karl Smith <ksmit218@utk.edu>
2017-02-06 17:56:51 -0500

    Updated TH2 converter, added comments.
    Now using matrix plot as it more closely resembles a TH2. This has
    both advantages and drawbacks. The last row and column are now properly
    rendered. Currently using `matrix plot*` as the no-starred version
    inverts the vertical axis. The plot does not permit holes in the matrix,
    so the z domain must be large enough to permit all data points. Using
    `restrict z to domain*` sets points outside the domain to the limit
    encountered. Changes to the view angle no longer make sense. Switching
    between `surf` and `matrix plot` requires that the bin coordinates
    change from the corner to the center.
    
    Added option string. If the string "surf" is passed the default `matrix
    plot*` is swapped with `surf` and the coordinates are adjusted.

--- TikzTh2.C
+++ TikzTh2.C
@@ -11,7 +11,12 @@
 
 #include "TH2.h"
 
-void TikzTh2(TH2* hist, std::string outputFilename="") {
+void TikzTh2(TH2* hist, std::string outputFilename="", std::string options="") {
+	bool surfPlot = false;
+	if (options.find("surf") != std::string::npos) {
+		surfPlot = true;
+	}
+
 	std::streambuf *buf = std::cout.rdbuf();
 
 	std::ofstream outputStream;
@@ -22,32 +27,60 @@ void TikzTh2(TH2* hist, std::string outputFilename="") {
 	std::ostream output(buf);
 
 	output << 
+		"\% Uses `matrix plot` which creates a filled patch at the middle\n"
+		"\% of the coordinates provided. The `surf` type plot is similar,\n"
+		"\% but expects the values of the corners to be provided. This   \n"
+		"\% requires an extra row and column of values are provided. This\n"
+		"\% would cause the plot to omit the final row and column of     \n"
+		"\% bins. The `matrix plot` does not permit any holes in the     \n"
+		"\% data, thus `restrict z to domain*` must be used which sets   \n"
+		"\% the z value for a point that exceeds the domain to the limit \n"
+		"\% it encountered.\n\n";
+		
+	output << 
 		"\\begin{tikzpicture}\n"
-		"\t\\begin{axis}[view={0}{90}, colorbar, \n"
+		"\t\\pgfplotsset{compat=1.3} \%Compatible with v1.3 and newer.\n" 
+		"\t\\begin{axis}[view={0}{90}, \%Top down view.\n"
+			"\t\txlabel={" << hist->GetXaxis()->GetTitle() <<  "},\n"
+			"\t\tylabel={" << hist->GetYaxis()->GetTitle() <<  "},\n"
+			"\t\tcolorbar, colorbar style={\n"
+			"\t\t\t\%To place the label on top of the colorbar use `title`.\n"
+			"\t\t\tylabel={" << hist->GetZaxis()->GetTitle() << "}\n"
+			"\t\t},\n"
 			"\t\txmin=" << hist->GetXaxis()->GetXmin() << ", "
 			"xmax=" << hist->GetXaxis()->GetXmax() << ",\n"
 			"\t\tymin=" << hist->GetYaxis()->GetXmin() << ", "
 			"ymax=" << hist->GetYaxis()->GetXmax() << ",\n"
+			"\t\t\%Colorbar limits:\n"
 			"\t\tpoint meta min=" << hist->GetMinimum() << ", "
 			"point meta max=" << hist->GetMaximum() << ",\n"
-			"\t\txlabel={" << hist->GetXaxis()->GetTitle() <<  "},\n"
-			"\t\tylabel={" << hist->GetYaxis()->GetTitle() <<  "},\n"
-			"\t\tzlabel={" << hist->GetZaxis()->GetTitle() <<  "}\n"
-		"\t]\n";
+			"\t\trestrict z to domain*=" << hist->GetBinContent(hist->GetMinimumBin()) << ":" << hist->GetBinContent(hist->GetMaximumBin()) << "]\n"
+		"\t]\n\n";
 
 	output <<
-		"\t\\addplot3[surf, "
-			"mesh/cols=" << hist->GetNbinsY() << ", "
-			"mesh/rows=" << hist->GetNbinsX() << "]\n"
+		"\t\\addplot3[";
+	if (surfPlot) output << "surf,";
+	else output <<	"matrix plot*, \%Similar to `surf`.";
+	output << "\n" 
+			"\t\tshader = flat corner, \n"
+			"\t\t\%Ordering of the coordinate data:\n"
+			"\t\tmesh/cols=" << hist->GetNbinsX() << ", "
+			"mesh/rows=" << hist->GetNbinsY() << ", "
+			"mesh/ordering=rowwise\n"
+		"\t]\n"
 		"\t\tcoordinates {\n";
 
-	for (int xbin=1; xbin<= hist->GetNbinsX(); xbin++) {
+	for (int ybin=1; ybin<= hist->GetNbinsY(); ybin++) {
 		output << "\t\t\t";
-		double xvalue = hist->GetXaxis()->GetBinLowEdge(xbin);
-		for (int ybin=1; ybin<= hist->GetNbinsY(); ybin++) {
-			double yvalue = hist->GetYaxis()->GetBinLowEdge(ybin);
-			double zvalue = hist->GetBinContent(xbin, ybin);
-			output << "(" << xvalue << "," << yvalue << "," << zvalue << ") ";
+		double yvalue;
+		if (surfPlot) yvalue = hist->GetYaxis()->GetBinLowEdge(ybin);
+		else yvalue = hist->GetYaxis()->GetBinCenter(ybin);
+		for (int xbin=1; xbin<= hist->GetNbinsX(); xbin++) {
+			double xvalue;
+			if (surfPlot) xvalue = hist->GetXaxis()->GetBinLowEdge(xbin);
+			else xvalue = hist->GetXaxis()->GetBinCenter(xbin);
+			double weight = hist->GetBinContent(xbin, ybin);
+			output << "(" << xvalue << "," << yvalue << "," << weight << ") ";
 		}
 		output << "\n";
 	}
