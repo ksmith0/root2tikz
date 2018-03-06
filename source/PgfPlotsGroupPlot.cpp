3e2cbcf94c9eaa9b257a5e2a934a03da38b73038
Karl Smith <ksmith@nuclearemail.org>
2018-03-06 10:09:55 -0700

    Improved identical label detection.

--- source/PgfPlotsGroupPlot.cpp
+++ source/PgfPlotsGroupPlot.cpp
@@ -1,11 +1,14 @@
 #include "PgfPlotsGroupPlot.hpp"
 
 #include <stdexcept>
+#include <vector>
 
-PgfPlotsGroupPlot::PgfPlotsGroupPlot(unsigned int rows, unsigned int columns, std::string options)
+PgfPlotsGroupPlot::PgfPlotsGroupPlot(unsigned int rows,
+												 unsigned int columns, std::string options)
 	: PgfPlotsAxis(options)
 {
-	options_["group style"] = "{rows=" + std::to_string(rows) + ", columns=" + std::to_string(columns) + "}";
+	options_["group style"] = "{rows=" + std::to_string(rows)
+									  + ", columns=" + std::to_string(columns) + "}";
 	subPlots_.resize(rows * columns, nullptr);
 }
 
@@ -18,7 +21,7 @@ void PgfPlotsGroupPlot::AddPlot(PgfPlotsPlot *plot, unsigned int plotId) {
 	GetSubPlot(plotId)->AddPlot(plot);
 }
 
-/** Get a pointer to the specified sub plot. This method witll create teh sub
+/** Get a pointer to the specified sub plot. This method will create the sub
  * plot object if it does not exist.
  *
  * \param[in] plotId the sub plot id to get the pointer for.
@@ -40,59 +43,65 @@ void PgfPlotsGroupPlot::PreprocessOptions() {
 	//Get the group style options.
 	std::string &groupStyleStr = options_["group style"];
 	TikzOptions groupStyle(groupStyleStr.substr(1, groupStyleStr.length() - 2));
-	int rows = std::stoi(groupStyle["rows"]);
-	int columns = std::stoi(groupStyle["columns"]);
+	const int rows = std::stoi(groupStyle["rows"]);
+	const int columns = std::stoi(groupStyle["columns"]);
 
-	//Identify if labels are all identical
+	//Determine column and row labels
+	std::vector<std::string> columnLabels(columns);
+	std::vector<std::string> rowLabels(rows);
 	bool identicalColumnLabels = true;
 	bool identicalRowLabels = true;
-	for (int column=0; column < columns; ++column) {
-		std::string columnLabel = GetSubPlot(columns * rows - columns + column)->GetOptions()->at("xlabel");
-		for (int row=0; row < rows; ++row) {
-			std::string rowLabel = GetSubPlot(columns * row)->GetOptions()->at("ylabel");
-			auto plotOptions = GetSubPlot(column * rows + row)->GetOptions();
-			if (identicalColumnLabels && plotOptions->at("xlabel") != columnLabel)
-			  identicalColumnLabels = false;
-			if (identicalRowLabels && plotOptions->at("ylabel") != rowLabel)
-			  identicalRowLabels = false;
+
+	for (int row=0; row < rows; ++row) {
+		for (int column=0; column < columns; ++column) {
+			auto subPlot = subPlots_.at(columns * row + column);
+			if (!subPlot) continue;
+
+			const auto &xlabel = subPlot->GetOptions()->at("xlabel");
+			const auto &ylabel = subPlot->GetOptions()->at("ylabel");
+
+			//Handle column labels (x-axis);
+			if (columnLabels[column] == "") columnLabels[column] = xlabel;
+			else if (xlabel != "" && columnLabels[column] != xlabel)
+				identicalColumnLabels = false;
+
+			//Handle row labels (y-axis);
+			if (rowLabels[row] == "") rowLabels[row] = ylabel;
+			else if (ylabel != "" && rowLabels[row] != ylabel)
+				identicalRowLabels = false;
 		}
 	}
+
+	//Currently cannot handle different labels for different columns or rows,
+	//but this could be corrected in the future.
+	for (auto label : columnLabels) {
+		if (label != "" && label != columnLabels[0]) identicalColumnLabels = false;
+	}
+	for (auto label : rowLabels) {
+		if (label != "" && label != rowLabels[0]) identicalRowLabels = false;
+	}
+
+	//Set the global labels
 	if (identicalColumnLabels)  {
-		options_.Add("xlabel=" + GetSubPlot(0)->GetOptions()->at("xlabel"));
+		options_.Add("xlabel=" + columnLabels[0]);
 		options_.Add("group/xlabels at=edge bottom");
 	}
 	if (identicalRowLabels) {
-		options_.Add("ylabel=" + GetSubPlot(0)->GetOptions()->at("ylabel"));
+		options_.Add("ylabel=" + rowLabels[0]);
 		options_.Add("group/ylabels at=edge left");
 	}
+
 	//Remove plot specific labels to avoid rendering for identical case.
-	for (int column=0; column < columns; ++column) {
-		for (int row=0; row < rows; ++row) {
-			auto plotOptions = GetSubPlot(column * rows + row)->GetOptions();
-			if (identicalColumnLabels) plotOptions->erase("xlabel");
-			if (identicalRowLabels) plotOptions->erase("ylabel");
-		}
+	for (auto subPlot : subPlots_) {
+		if (!subPlot) continue;
+		auto plotOptions = subPlot->GetOptions();
+		if (identicalColumnLabels) plotOptions->erase("xlabel");
+		if (identicalRowLabels) plotOptions->erase("ylabel");
 	}
 
 	//Determine if spacing needs to be increased.
-	for (int column=1; column < columns && !options_.IsDefined("group/horizontal sep"); ++column) {
-		for (int row=0; row < rows; ++row) {
-			auto plotOptions = GetSubPlot(column * rows + row)->GetOptions();
-			if (plotOptions->IsDefined("ylabel")) {
-				options_.Add("group/horizontal sep=2cm");
-				break;
-			}
-		}
-	}
-	for (int row=1; row < rows && !options_.IsDefined("group/vertical sep"); ++row) {
-		for (int column=0; column < columns; ++column) {
-			auto plotOptions = GetSubPlot(column * rows + row)->GetOptions();
-			if (plotOptions->IsDefined("xlabel")) {
-				options_.Add("group/vertical sep=2cm");
-				break;
-			}
-		}
-	}
+	if (!identicalColumnLabels) options_.Add("group/vertical sep=2cm");
+	if (!identicalRowLabels) options_.Add("group/horizontal sep=2cm");
 }
 
 /** Write the registered plots to the specified buffer.
